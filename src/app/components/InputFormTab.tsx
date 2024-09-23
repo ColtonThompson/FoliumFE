56371fc66f152c28fc83571c00b13f67ac74ab2a
Colton Thompson <colton.thompson@live.com>
2024-09-23 12:06:48 -0600

    Updated InputFormTab.tsx
    The InputFormTab.tsx now uses InputMask for Zip code entry instead of InputText

--- src/app/components/InputFormTab.tsx
+++ src/app/components/InputFormTab.tsx
@@ -1,32 +1,24 @@
 ﻿"use client"
-import React, {useState} from "react";
-import {InputText} from "primereact/inputtext";
+import React from "react";
+import {InputMask} from "primereact/inputmask";
 
 export default function MyInputFormTab() {
     const [zipCode, setZipCode] = React.useState("");
-    const [isValid, setIsValid] = useState(true);
-
-    const validateInput = (event: { target: { value: any; }; }) => {
-        const newZipCode = event.target.value;
-        setZipCode(newZipCode);
-
-        // Regular expression for US zip codes (5 digits or 5+4 digits)
-        const zipCodeRegex = /^\d{5}(-\d{4})?$/;
-        setIsValid(zipCodeRegex.test(newZipCode));
-    };
 
     return (
         <React.Fragment>
-            <label>Zip Code Test Field</label>
-            <br/>
-            <InputText id="zip"
-                       value={zipCode}
-                       onChange={validateInput}
-                       placeholder="Enter Zip Code"
-            />
-            <br/>
-            <br/>
-            {!isValid && <p>Invalid Zip Code format!</p>}
+            <div className="card flex justify-content-center">
+                <div>
+                    <label htmlFor="zip" className="font-bold block mb-2">
+                        Zip Code Example:
+                    </label>
+                    <InputMask id="zip" value={zipCode} onChange={(e) => setZipCode(e.target.value)} mask="99999? -9999"
+                               placeholder="12345-1234"
+                    />
+                    <br/>
+                    You entered a zip code for: {zipCode}
+                </div>
+            </div>
         </React.Fragment>
     )
 }
