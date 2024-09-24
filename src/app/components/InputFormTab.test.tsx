acd70e3eb7bbe56e38492ae3652afa9c81c0ab5a
Colton Thompson <colton.thompson@live.com>
2024-09-24 17:10:32 -0600

    Fixes to swc being used for compilation breaking the app
    - Added forceSwcTransform option to next.config.mjs

--- src/app/components/InputFormTab.test.tsx
+++ src/app/components/InputFormTab.test.tsx
@@ -1,6 +1,5 @@
 ﻿import React from 'react';
-import {render, screen} from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
+import {render} from '@testing-library/react'
 import '@testing-library/jest-dom'
 import MyInputFormTab from './InputFormTab';
 
